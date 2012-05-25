78aa18425d93b0668dd4442c70125a419dd585b6
seannicholls <seannicholls@gmail.com>
2012-05-25 15:16:09 +0100

    fixed bugs inserted by move over to multi-object model, now in an unstable, but testable state.

--- test/test.js
+++ test/test.js
@@ -23,12 +23,33 @@ twitter.authenticate(	'consumer_key',
 
 // get a user's timeline
 twitter.timeline.user({ screen_name: 'sean_nicholls' }, function(result) {
-	console.log('-----------------[ twitter.timeline.user ]-----------------');
-	testUtil.summariseTwitter(result);
+
+	printHRule();
+	
+	if(result.isSuccess) {
+		testUtil.summariseTwitter(result.message());
+	} else {
+		console.log('Error: ' + result.message());
+	}
+	
 });
 
-// get users who retweeted a status
-twitter.retweets.statuses.by_user({ id: '205263849622999040' }, function(result) {
-	console.log('-----------------[ twitter.retweets.statuses.by_user ]-----------------');
-	testUtil.summariseTwitter(result);
+
+twitter.retweets.statuses.by_user({ id: '205263849622999040' }, function(result, parent) {
+
+	printHRule();
+
+	if(result.isSuccess) {
+		testUtil.summariseTwitter(result.message());
+	} else {
+		console.log('Error: ' + result.message());
+	}
+	
 });
+
+
+function printHRule() {
+
+	console.log('-------------------');
+	
+}
