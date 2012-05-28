61eda5e735d9daa1e8029303a16e1abac795af3c
seannicholls <seannicholls@gmail.com>
2012-05-28 19:40:43 +0100

    oauth authentication works, round robin works, unauthenticated rest calls work, all appears in order for baseline functionality
    Signed-off-by: seannicholls <seannicholls@gmail.com>

--- test/test.js
+++ test/test.js
@@ -8,20 +8,49 @@
  *
 */
 var	Twitter = require('../lib/twitter'),
-	TestUtil = require('./util');
+	TestUtil = require('./util'),
+	util = require('util');
 				
 var testUtil = new TestUtil(),
-	twitter = new Twitter(),
-	anotherTwitter = new Twitter();
+	twitter = new Twitter();
 
-// authenticate
-twitter.authenticate(	'consumer_key',
-						'consumer_secret',
-						'access_token_key',
-						'access_token_secret' );
+// NOTE: change the following to match your application settings!
+var credential = new Twitter.LoginCredential(	'consumer_key',
+												'consumer_secret',
+												'access_token_key',
+												'access_token_secret'	);
+
+authorisedTwitter = new Twitter(credential);
+
+
+// get the public timeline
+authorisedTwitter.timeline.public( {}, function(result, parent) {
+
+	printHRule();
+
+	if(result.isSuccess) {
+		testUtil.summariseTwitter(result.message(), '(public_timeline) ');
+	} else {
+		printError(result.error); 
+	}
+	
+});
+
+// get the users who retweeted a tweet with the specfied id
+twitter.retweets.statuses.by_user({ id: '205263849622999040' }, function(result, parent) {
+
+	printHRule();
+
+	if(result.isSuccess) {
+		testUtil.summariseTwitter(result.message(), '(retweeted by) ');
+	} else {
+		printError(result.error); 
+	}
+	
+});
 
 // get a user's timeline, including retweets
-twitter.timeline.user({ screen_name:	'sean_nicholls',
+authorisedTwitter.timeline.user({ screen_name:	'sean_nicholls',
 						include_rts:	'true' },
 						function(result) {
 
@@ -35,34 +64,21 @@ twitter.timeline.user({ screen_name:	'sean_nicholls',
 	
 });
 
-// get the users who retweeted a tweet with the specfied id
-twitter.retweets.statuses.by_user({ id: '205263849622999040' }, function(result, parent) {
+/*
+// send a tweet (this should produce an error, without authentication);
+authorisedTwitter.statuses.create.standard( {status:	'hello world! this is a test of the Node.js twitter library on @github https://github.com/snicholls/TwitterRestClient'}, function(result, parent) {
 
 	printHRule();
 
-	if(result.isSuccess) {
-		testUtil.summariseTwitter(result.message(), '(retweeted by) ');
-	} else {
-		console.log('Error: ' + result.message());
-	}
-	
+	console.log(result);
+
 });
-
-// get the public timeline
-anotherTwitter.timeline.public( null, function(result, parent) {
-
-	printHRule();
-
-	if(result.isSuccess) {
-		testUtil.summariseTwitter(result.message(), '(public_timeline) ');
-	} else {
-		console.log('Error: ' + result.message());
-	}
-	
-});
+*/
 
 function printHRule() {
-
 	console.log('-------------------');
-	
+}
+
+function printError(error) {
+	console.log('Error (' + error.code + '): ' + error.message);
 }
