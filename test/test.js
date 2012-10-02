99fd299aa82b027ca2ce11d69226f74404c365d5
Sean Nicholls <seannicholls@gmail.com>
2012-10-02 21:09:08 +0100

    converted tabs to spaces, reverted package.json to correct version
    Signed-off-by: Sean Nicholls <seannicholls@gmail.com>

--- test/test.js
+++ test/test.js
@@ -1,18 +1,18 @@
 /*
- * Author:			Sean Nicholls
- * Website:			http://www.seannicholls.com
- * github:			https://github.com/snicholls
- * Twitter:			@sean_nicholls
+ * Author:          Sean Nicholls
+ * Website:         http://www.seannicholls.com
+ * github:          https://github.com/snicholls
+ * Twitter:         @sean_nicholls
  *
- * Assumptions:		Your brother is named tim and has an eye patch.
+ * Assumptions:     Your brother is named tim and has an eye patch.
  *
 */
-var	Twitter = require('../lib/twitter'),
-	util = require('util');
+var Twitter = require('../lib/twitter'),
+    util = require('util');
 
-//	
+//  
 // The twitter object we're going to use
-//			
+//          
 var twitter = new Twitter();
 
 //
@@ -20,15 +20,15 @@ var twitter = new Twitter();
 //
 var util = new Twitter.util();
 
-// NOTE:	change the following to match your application settings!
-// 			THE TWITTER STREAMING API WILL NOT WORK WITHOUT SUPPLYING
-//			A VALID (NON-ANOYMOUS) AUTHENTICATION CREDENTIAL
+// NOTE:    change the following to match your application settings!
+//          THE TWITTER STREAMING API WILL NOT WORK WITHOUT SUPPLYING
+//          A VALID (NON-ANOYMOUS) AUTHENTICATION CREDENTIAL
 //
 /*
-var credential = new Twitter.LoginCredential(	'CONSUMER_KEY',
-												'CONSUMER_SECRET',
-												'ACCESS_TOKEN',
-												'ACCESS_TOKEN_SECRET'	);
+var credential = new Twitter.LoginCredential(   'CONSUMER_KEY',
+                                                'CONSUMER_SECRET',
+                                                'ACCESS_TOKEN',
+                                                'ACCESS_TOKEN_SECRET'   );
 */
 var credential = new Twitter.LoginCredential();
 
@@ -49,7 +49,7 @@ authorisedTwitter.timeline.public( {}, function(result, parent) {
     } else {
         console.log("Error: " + result.message); 
     }
-	
+    
     console.log('---------------------------');
     
 });
@@ -66,15 +66,15 @@ twitter.retweets.statuses.by_user({ id: '205263849622999040' }, function(result,
     } else {
         console.log("Error: " + result.message()); 
     }
-	
+    
     console.log('---------------------------');
     
 });
 
 // get a user's timeline, including retweets
-authorisedTwitter.timeline.user({ 	screen_name:	'BarackObama',
-									include_rts:	'false' },
-									function(result) {
+authorisedTwitter.timeline.user({   screen_name:    'BarackObama',
+                                    include_rts:    'false' },
+                                    function(result) {
 
     if(result.isSuccess) {
         var tweets = result.message();
@@ -84,7 +84,7 @@ authorisedTwitter.timeline.user({ 	screen_name:	'BarackObama',
     } else {
         console.log("Error: " + result.message()); 
     }
-	
+    
     console.log('---------------------------');
     
 });
@@ -92,10 +92,10 @@ authorisedTwitter.timeline.user({ 	screen_name:	'BarackObama',
 /* 
 // ENABLE AT YOUR OWN RISK!
 // send a tweet (this should produce an error without authentication);
-authorisedTwitter.statuses.create.standard( { status:	'hello world! this is a test of the #Node.js twitter library, on #github at https://github.com/snicholls/TwitterRestClient'},
-											function(result, parent) {
+authorisedTwitter.statuses.create.standard( { status:   'hello world! this is a test of the #Node.js twitter library, on #github at https://github.com/snicholls/TwitterRestClient'},
+                                            function(result, parent) {
 
-	console.log(result);
+    console.log(result);
 
 });
 */
@@ -107,50 +107,50 @@ authorisedTwitter.statuses.create.standard( { status:	'hello world! this is a te
 
 if(credential.isAnonymous) {
 
-	console.log("Cannot test the Twitter streaming API without an authenticated login.");
-	
-} else {
-	
-	// sample the public timeline
-	authorisedTwitter.stream.statuses.sample({}, function(result) {
-	
-		if(result.isSuccess) {
-		
-			try {
-				var tweet = JSON.parse(result.data);
-				console.log(util.summariseTwitter(tweet, '(sample) '));
-			} catch (e) {
-				// best to just ignore it, eh?
-				console.log(e);
-			}
-			
-		} else {
-			
-			console.log("Error: " + result.message);
-			
-		}
-		
-	});
-	
-	/*
-    // track some keywords
-    authorisedTwitter.stream.statuses.filter(	{ track: ["Twitter","Facebook","YouTube"] },
-    											function(result) {
+    console.log("Cannot test the Twitter streaming API without an authenticated login.");
     
-    	if(result.isSuccess) {
-    		
-    		try {
-    			var tweet = JSON.parse(result.data);
-    			console.log(util.summariseTwitter(tweet, '(filter) '));
-    		} catch (e) {
-    			// best to just ignore it, eh?
-    		}
-    		
-    	} else {
-    		
-    		console.log(result.error.message);
-    		
-    	}
+} else {
+    
+    // sample the public timeline
+    authorisedTwitter.stream.statuses.sample({}, function(result) {
+    
+        if(result.isSuccess) {
+        
+            try {
+                var tweet = JSON.parse(result.data);
+                console.log(util.summariseTwitter(tweet, '(sample) '));
+            } catch (e) {
+                // best to just ignore it, eh?
+                console.log(e);
+            }
+            
+        } else {
+            
+            console.log("Error: " + result.message);
+            
+        }
+        
+    });
+    
+    /*
+    // track some keywords
+    authorisedTwitter.stream.statuses.filter(   { track: ["Twitter","Facebook","YouTube"] },
+                                                function(result) {
+    
+        if(result.isSuccess) {
+            
+            try {
+                var tweet = JSON.parse(result.data);
+                console.log(util.summariseTwitter(tweet, '(filter) '));
+            } catch (e) {
+                // best to just ignore it, eh?
+            }
+            
+        } else {
+            
+            console.log(result.error.message);
+            
+        }
     });
     */
 }
